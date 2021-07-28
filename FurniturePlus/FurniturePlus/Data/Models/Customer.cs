978ba57cd09d6895d1d627083a1b143c821d2b7b
unknown <pavelmitev@yahoo.com>
2021-07-28 19:15:35 +0300

    Adding Customer class

--- FurniturePlus/FurniturePlus/Data/Models/Customer.cs
+++ FurniturePlus/FurniturePlus/Data/Models/Customer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FurniturePlus.Data.Models
+﻿namespace FurniturePlus.Data.Models
 {
     public class Customer : User
     {
